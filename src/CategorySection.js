53584f52f821b6177f964da76e55e638c7bf2760
Sam Brown <jumpbro@hotmail.com>
2022-08-16 18:44:47 +1000

    comments

--- src/CategorySection.js
+++ src/CategorySection.js
@@ -5,11 +5,12 @@ import fruitIcon from './images/fruit.png'
 import vegetableIcon from './images/vegetable.png'
 import dairyIcon from './images/dairy.png'
 
+// Creates the category section containing the category filtering buttons
 function CategorySection({ setFiltering, updateCategory }){
-
     return(
         <div className="CategorySection">
             <h2>Filter By Category</h2>
+            {/* On button click run the updateCategory function to filter the list */}
             <div className="Buttons">
                 <CategoryButton onClick={() => updateCategory("Meat")} icon={meatIcon} />
                 <CategoryButton onClick={() => updateCategory("Fruit")} icon={fruitIcon} />
